8252752a47b15a98af998522575fd245e104d7dd
kutman <kutman@MacBook-Pro-kutman.local>
2020-12-01 18:13:51 +0600

    unlike the post feature

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -20,7 +20,13 @@ def CategoryView(request,cats):
 
 def LikeView(request,pk):
 	post = get_object_or_404(Post, id=request.POST.get('post_id'))
-	post.likes.add(request.user)
+	liked = False
+	if post.likes.filter(id=request.user.id).exists():
+		post.likes.remove(request.user)
+		liked = False
+	else:
+		post.likes.add(request.user)
+		liked = True
 	return HttpResponseRedirect(reverse('post-detail',args=[str(pk)]))
 	
 
@@ -44,9 +50,13 @@ class PostDetailView(DetailView):
 		cat_menu = Category.objects.all()
 		stuff = get_object_or_404(Post, id = self.kwargs['pk'])
 		total_likes = stuff.total_llikes()
+		liked = False
+		if stuff.likes.filter(id=self.request.user.id).exists():
+			liked = True
 		context = super(PostDetailView,self).get_context_data(*args,**kwargs)
 		context['cat_menu'] = cat_menu
 		context['total_likes'] = total_likes
+		context['liked'] = liked
 		return context
 
 
