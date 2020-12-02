e7e00c637d007e52769e9d66bf7086ee6e5e502a
kutman <kuu.life@gmail.com>
2020-12-03 01:34:12 +0600

    post_picture and soc links

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -14,6 +14,19 @@ class Category(models.Model):
 	def get_absolute_url(self):
 		return reverse('home')
 
+class Profile(models.Model):
+	user  = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
+	profile_image = models.ImageField(null = True,blank = True, upload_to = 'images-profile/')
+	instagram_urls = models.CharField(max_length = 200, null = True, blank = True)
+	website_urls = models.CharField(max_length = 200, null = True, blank = True)
+	github_urls = models.CharField(max_length = 200, null = True, blank = True)
+	twitter_urls = models.CharField(max_length = 200, null = True, blank = True)
+	facebook_urls = models.CharField(max_length = 200, null = True, blank = True)
+
+
+
+	def __str__(self):
+		return str(self.user)
 
 class Post(models.Model):
 	title = models.CharField(max_length=200)
